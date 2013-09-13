5863a03d6e67ab7e48bf2abf526e2d816045a2d1
Daria Jung <djj2115cu@gmail.com>
2013-09-13 14:05:56 -0400

    start styling content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+$(function () {
 
+	var xhr = $.get("resources.json");
+	xhr.done(function(data) { 
+        console.log("success got data", data);
+        var html = ''; 
+        console.log(data.paths[0].name);
+        for (var i = 0; i < data.paths.length; i++) {
+           	html += '<h2>' + data.paths[i].name + '</h2>';
+           	html += '<p class="path-topics">' + data.paths[i].topics + '</div>';
+        }
+        $('.paths').append(html);
+
+        html = '';
+        for (var i = 0; i < data.topics.length; i++) {
+        	html += '<h2>' + data.topics[i].name + '</h2>';
+        	html += '<p class="description">' + data.topics[i].description + '</div>';
+        	html += '<ul class="resources">';
+        	for (var j = 0; j < data.topics[i].resources.length; j++) {
+        		html += '<li><a target="_blank" href=' + data.topics[i].resources[j].url + '>' + data.topics[i].resources[j].name + '</a></li>';
+        	}
+        	html += '</ul>';
+        }
+        $('.topics').append(html);
+    });	
+	
+});
