8b07b69807f8199b87081b280f93de894df04a97
Daria Jung <djj2115cu@gmail.com>
2013-09-15 15:26:45 -0400

    add accordion animation

--- js/main.js
+++ js/main.js
@@ -9,25 +9,39 @@ $(function () {
         var html = ''; 
         console.log(data.paths[0].name);
         for (var i = 0; i < data.paths.length; i++) {
-           	html += '<h2>' + data.paths[i].name + '</h2>';
+           	html += '<h2 class="path-name">' + data.paths[i].name + '</h2>';
+            html += '<div class="path-topics-wrapper">';
            	for (var j = 0; j < data.paths[i].topics.length; j++) {
-           		html += '<p class="path-topics">' + data.paths[i].topics[j] + '</div>';
+           		html += '<div class="path-topics">' + data.paths[i].topics[j] + '</div>';
             }
+            html += '</div>';
         }
         $('.paths').append(html);
 
         html = '';
         for (var i = 0; i < data.topics.length; i++) {
-        	html += '<h2>' + data.topics[i].name + '</h2>';
-        	html += '<p class="description">' + data.topics[i].description + '</div>';
-        	html += '<ul class="resources">';
+        	html += '<h2 class="topic">' + data.topics[i].name + '</h2>';
+        	html += '<div class="description">' + data.topics[i].description + '</div>';
+        	html += '<div class="resources">';
         	for (var j = 0; j < data.topics[i].resources.length; j++) {
         		html += '<li><a target="_blank" href=' + data.topics[i].resources[j].url + '>' 
         		+ data.topics[i].resources[j].name + '</a> &#8212; ' +  data.topics[i].resources[j].description + '</li>';
         	}
-        	html += '</ul>';
+        	html += '</div>';
         }
         $('.topics').append(html);
+
+        $('.topic').on('click', function (event) {
+            var topic = $(event.currentTarget);
+            if (! topic.hasClass('active')){
+              topic.addClass('active');
+              topic.next('.description').next('.resources').slideDown("slow");
+            } 
+            else if (topic.hasClass('active')){
+              topic.removeClass('active');
+               topic.next('.description').next('.resources').slideUp("slow");
+            }  
+        });
     });	
 	
 });
