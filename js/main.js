47b09915dc6ff70d7c4347be146b0c699c71086c
Daria Jung <djj2115cu@gmail.com>
2013-09-15 17:12:21 -0400

    change tabs to spaces

--- js/main.js
+++ js/main.js
@@ -16,16 +16,16 @@ $(function () {
         zIndex: 2147483647 
     });
 
-	$('.jumbotron h1, .jumbotron p').delay(100).animate({ opacity: 1 }, 800);
+    $('.jumbotron h1, .jumbotron p').delay(100).animate({ opacity: 1 }, 800);
 
-	var xhr = $.get("resources.json");
-	xhr.done(function(data) { 
+    var xhr = $.get("resources.json");
+    xhr.done(function(data) { 
         var html = ''; 
         for (var i = 0; i < data.paths.length; i++) {
-           	html += '<h2 class="path-name">' + data.paths[i].name + '</h2>';
+            html += '<h2 class="path-name">' + data.paths[i].name + '</h2>';
             html += '<div class="path-topics-wrapper">';
-           	for (var j = 0; j < data.paths[i].topics.length; j++) {
-           		html += '<div class="path-topics">' + data.paths[i].topics[j] + '</div>';
+            for (var j = 0; j < data.paths[i].topics.length; j++) {
+                html += '<div class="path-topics">' + data.paths[i].topics[j] + '</div>';
             }
             html += '</div>';
         }
@@ -33,28 +33,28 @@ $(function () {
 
         html = '';
         for (var i = 0; i < data.topics.length; i++) {
-        	html += '<h2 class="topic">' + data.topics[i].name + '</h2>';
-        	html += '<div class="description">' + data.topics[i].description + '</div>';
-        	html += '<div class="resources">';
-        	for (var j = 0; j < data.topics[i].resources.length; j++) {
-        		html += '<li><a target="_blank" href=' + data.topics[i].resources[j].url + '>' 
-        		+ data.topics[i].resources[j].name + '</a> &#8212; ' +  data.topics[i].resources[j].description + '</li>';
-        	}
-        	html += '</div>';
+            html += '<h2 class="topic">' + data.topics[i].name + '</h2>';
+            html += '<div class="description">' + data.topics[i].description + '</div>';
+            html += '<div class="resources">';
+            for (var j = 0; j < data.topics[i].resources.length; j++) {
+                html += '<li><a target="_blank" href=' + data.topics[i].resources[j].url + '>' 
+                + data.topics[i].resources[j].name + '</a> &#8212; ' +  data.topics[i].resources[j].description + '</li>';
+            }
+            html += '</div>';
         }
         $('.topics').append(html);
 
         $('.topic').on('click', function (event) {
             var topic = $(event.currentTarget);
             if (! topic.hasClass('active')){
-              topic.addClass('active');
-              topic.next('.description').next('.resources').slideDown("slow");
+                topic.addClass('active');
+                topic.next('.description').next('.resources').slideDown("slow");
             } 
             else if (topic.hasClass('active')){
-              topic.removeClass('active');
-               topic.next('.description').next('.resources').slideUp("slow");
+                topic.removeClass('active');
+                topic.next('.description').next('.resources').slideUp("slow");
             }  
         });
-    });	
-	
+    }); 
+    
 });
