ac7417751ab70976b13e7c48ea562ff47d56b390
Robert Ying <rbtying@aeturnalus.com>
2013-09-20 14:30:44 -0400

    Animated more stuff

--- js/app.js
+++ js/app.js
@@ -3,8 +3,9 @@
 'use strict'
 
 angular.module('learnApp', ['ngAnimate'])
-    .controller('BaseCtrl', ['$log', '$http', '$location', '$anchorScroll', '$timeout', '$scope', function($log, $http, $location, $anchorScroll, $timeout, $scope) {
+    .controller('BaseCtrl', ['$log', '$http', '$q', '$location', '$anchorScroll', '$timeout', '$scope', function($log, $http, $q, $location, $anchorScroll, $timeout, $scope) {
         $scope.animations = {};
+        $scope.topicLookupTable = {};
 
         $http.get('resources.json').success(function(data) {
             $scope.paths = data.paths;
@@ -13,6 +14,7 @@ angular.module('learnApp', ['ngAnimate'])
             $scope.topics.topicIds = {};
             angular.forEach($scope.topics, function(topic, idx) {
                 $scope.topics.topicIds[topic.name] = topic.name.replace(/[^a-zA-Z]/g, '').toLowerCase();
+                $scope.topicLookupTable[topic.name] = topic;
             });
 
         }).error(function(data) {
@@ -24,10 +26,26 @@ angular.module('learnApp', ['ngAnimate'])
             $scope.animations.showJumbotron = true;
         }, 100);
 
+        $scope.scrollToElement = function(selector, time, verticalOffset) {
+            var promise = $q.defer();
+            time = typeof(time) != 'undefined' ? time : 1000;
+            verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+            var element = $(selector);
+            var offset = element.offset();
+            var offsetTop = offset.top + verticalOffset;
+            $('html, body').animate({
+                scrollTop: offsetTop
+            }, time, function() {
+                promise.resolve();
+            });
+
+            return promise.promise;
+        };
+
         $scope.activateTopic = function(topic) {
-            $log.log('activating', topic);
-            $location.hash($scope.topics.topicIds[topic]);
-            $anchorScroll();
+            $scope.scrollToElement('#' + $scope.topics.topicIds[topic], 500, 0).then(function(){
+                $scope.topicLookupTable[topic].active = true;
+            });
         };
     }])
 }());
